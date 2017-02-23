02705f2f4d67eab1dd5bad6b479dfee755df56bd
Sara <sara.szajewska@gmail.com>
2017-02-23 10:49:32 +0100

    knapp etc

--- ataleuntold_home.js
+++ ataleuntold_home.js
@@ -6,16 +6,6 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 } 
 
-//sidenav
-/*
-let sidenavButton = document.getElementsByClassName('sidenavButton'),
-    sidenav = document.getElementsByClassName('sidenav');
-
-for (let i = 0; i < sidenavButton.length; i++){
-    sidenavButton[i].addEventListener('click', ()=>{
-        sidenav[0].classList.toggle('sidenavOpen');
-    });
-}*/
 
 
 
